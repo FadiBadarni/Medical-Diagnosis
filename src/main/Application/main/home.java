68c6174d5fd5574b2a05c9eaacd21b05fa44bee3
fadi badarni <fadybd1@gmail.com>
2022-05-01 16:28:19 +0300

    updates from master

--- src/main/Application/main/home.java
+++ src/main/Application/main/home.java
@@ -1,16 +1,22 @@
 package main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 
@@ -79,5 +85,14 @@ public class home implements Initializable {
     @FXML
     private void sendData(ActionEvent event) {
 
+    }
+
+    public void displayInfoButton_Click(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("patientInfo.fxml")));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
     }
 }
